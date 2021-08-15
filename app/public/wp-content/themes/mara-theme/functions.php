82bcc129a7fbf4d1923162cf7db315473a39b119
Septiana Yoga <septianayoga30@gmail.com>
2021-08-15 14:07:21 +0700

    updating minor

--- app/public/wp-content/themes/mara-theme/functions.php
+++ app/public/wp-content/themes/mara-theme/functions.php
@@ -10,7 +10,8 @@ function university_files()
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features() {
+function university_features()
+{
   add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'university_features');
