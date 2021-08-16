eff3dc6b3a560b091f4ac14e74e491f3c0814474
Septiana Yoga <septianayoga30@gmail.com>
2021-08-16 17:33:54 +0700

    adding event menu

---
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,17 @@
+<?php
+function university_post_types()
+{
+  register_post_type('event', array(
+    'public' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add new event',
+      'edit_item' => 'Edit event',
+      'all_items' => 'All Event',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
+}
+
+add_action('init', 'university_post_types');
