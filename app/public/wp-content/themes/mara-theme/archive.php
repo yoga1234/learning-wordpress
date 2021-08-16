77404d65b913856cfbad9f04ef8b71622c321249
Septiana Yoga <septianayoga30@gmail.com>
2021-08-16 11:36:04 +0700

    updating archive pages

--- app/public/wp-content/themes/mara-theme/archive.php
+++ app/public/wp-content/themes/mara-theme/archive.php
@@ -10,7 +10,7 @@ get_header();
       ?>
     </h1>
     <div class="page-banner__intro">
-      <p>Keep up with our latest news.</p>
+      <p><?php the_archive_description(); ?></p>
     </div>
   </div>
 </div>
