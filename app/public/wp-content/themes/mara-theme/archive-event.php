4adc52c09d2e9330c96cd39eb6336c64b15dade6
Septiana Yoga <septianayoga30@gmail.com>
2021-08-18 14:01:28 +0700

    event page update link and styles

--- app/public/wp-content/themes/mara-theme/archive-event.php
+++ app/public/wp-content/themes/mara-theme/archive-event.php
@@ -17,14 +17,14 @@ get_header();
   <?php
   while (have_posts()) {
     the_post(); ?>
-    <div class="post-item">
-      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <div class="metabox">
-        <p>Posted By <?php the_author_posts_link(); ?> on <?php the_time('j-n-y'); ?> in <?php echo get_the_category_list(', ') ?></p>
-      </div>
-      <div class="generic-content">
-        <?php the_excerpt(); ?>
-        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+    <div class="event-summary">
+      <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+        <span class="event-summary__month"><?php the_time('M'); ?></span>
+        <span class="event-summary__day"><?php the_time('d'); ?></span>
+      </a>
+      <div class="event-summary__content">
+        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+        <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
       </div>
     </div>
   <?php
--- app/public/wp-content/themes/mara-theme/single-event.php
+++ app/public/wp-content/themes/mara-theme/single-event.php
@@ -15,7 +15,7 @@ while (have_posts()) {
   <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
       <p>
-        <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span class="metabox__main"><?php the_title(); ?></span>
+        <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span class="metabox__main"><?php the_title(); ?></span>
       </p>
     </div>
     <div class="generic-content">
