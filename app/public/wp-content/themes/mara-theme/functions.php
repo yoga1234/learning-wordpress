173ca86a248bde3901b82a6a112a0a1ebcccc184
Septiana Yoga <septianayoga30@gmail.com>
2021-08-20 22:12:27 +0700

    minor edit on function for checking uploaded images

--- app/public/wp-content/themes/mara-theme/functions.php
+++ app/public/wp-content/themes/mara-theme/functions.php
@@ -11,7 +11,7 @@ function pageBanner($args = NULL)
   }
 
   if (!$args['photo']) {
-    if (get_field('page_banner_background_image')) {
+    if (get_field('page_banner_background_image') AND !is_archive() AND !is_home()) {
       $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
     } else {
       $args['photo'] = get_theme_file_uri('images/ocean.jpg');
