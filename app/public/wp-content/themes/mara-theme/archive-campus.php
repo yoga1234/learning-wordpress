d0c910b4d58ea6bb2939941c1ab66a0fd0033859
Septiana Yoga <septianayoga30@gmail.com>
2021-08-21 21:21:29 +0700

    add location for campuses location

---
+++ app/public/wp-content/themes/mara-theme/archive-campus.php
@@ -0,0 +1,29 @@
+<?php
+get_header();
+
+pageBanner(array(
+  'title' => 'Our Campuses',
+  'subtitle' => 'We have several conveniently located campuses.'
+));
+?>
+
+<div class="container container--narrow page-section">
+  <ul class="link-list min-list">
+    <?php
+    while (have_posts()) {
+      the_post(); ?>
+      <li>
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        <p><?php the_field('map_location'); ?></p>
+      </li>
+    <?php
+    }
+
+    echo paginate_links();
+    ?>
+  </ul>
+</div>
+
+<?php
+get_footer();
+?>
