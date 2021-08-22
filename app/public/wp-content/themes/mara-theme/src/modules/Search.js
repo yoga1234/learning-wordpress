d0b8bcdb507b9f6679ff46db4a84838aa49b5636
Septiana Yoga <septianayoga30@gmail.com>
2021-08-22 20:35:57 +0700

    adding behaviour into livesearch UI

--- app/public/wp-content/themes/mara-theme/src/modules/Search.js
+++ app/public/wp-content/themes/mara-theme/src/modules/Search.js
@@ -6,17 +6,26 @@ class Search {
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
   // 2. events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
     $(document).on("keyup", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keyup", this.typingLogic.bind(this));
   }
 
   // 3. methods (functions, action...)
+  typingLogic() {
+    clearTimeout(this.typingTimer);
+    F;
+    this.typingTimer = setTimeout(x, 2000);
+  }
+
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOverlayOpen) {
       this.openOverlay();
