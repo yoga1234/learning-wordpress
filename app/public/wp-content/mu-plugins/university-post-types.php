643be51e2e9d64229e976255f9a4b3700d4da3d2
Septiana Yoga <septianayoga30@gmail.com>
2021-08-24 11:13:58 +0700

    minor changes on rest api

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -52,7 +52,7 @@ function university_post_types()
   ));
 
   // Professor Post Type
-  register_post_type('Professor', array(
+  register_post_type('professor', array(
     'show_in_rest' => true,
     'supports' => array('title', 'editor', 'thumbnail'),
     'public' => true,
