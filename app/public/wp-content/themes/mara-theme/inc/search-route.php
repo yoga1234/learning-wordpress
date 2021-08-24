4db41a3e84076a75e6994116d5d5af46e3cd6b72
Septiana Yoga <septianayoga30@gmail.com>
2021-08-24 11:47:49 +0700

    finishing simple search route on the rest api

--- app/public/wp-content/themes/mara-theme/inc/search-route.php
+++ app/public/wp-content/themes/mara-theme/inc/search-route.php
@@ -11,5 +11,19 @@ function university_register_search()
 
 function universitySearchResults()
 {
-  return 'This is a new route';
+  $professors = new WP_Query(array(
+    'post_type' => 'professor'
+  ));
+
+  $professorsResults = array();
+
+  while ($professors->have_posts()) {
+    $professors->the_post();
+    array_push($professorsResults, array(
+      'title' => get_the_title(),
+      'permalink' => get_the_permalink()
+    ));
+  }
+
+  return $professorsResults;
 }
